b0e5c316d81959ee503ed01c97d3dbfa15cc3d14
doloreses <popokaja9@gmail.com>
2022-09-22 11:22:14 +0200

    szkola

--- src/Nauczyciel.java
+++ src/Nauczyciel.java
@@ -1,3 +1,8 @@
-public class Nauczyciel extends Osoba permits Wychowawca{
+public sealed class Nauczyciel extends Osoba permits Wychowawca{
     private String przedmiot;
+
+    public Nauczyciel(String imie, int wiek, String przedmiot) {
+        super(imie, wiek);
+        this.przedmiot = przedmiot;
+    }
 }
--- src/Wychowawca.java
+++ src/Wychowawca.java
@@ -1,2 +1,7 @@
-public class Wychowawca {
+public final class Wychowawca extends Nauczyciel{
+    //ToDo pole określające jakiej klasy dotyczy
+
+    public Wychowawca(String imie, int wiek, String przedmiot) {
+        super(imie, wiek, przedmiot);
+    }
 }
